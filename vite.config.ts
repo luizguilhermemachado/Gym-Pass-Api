c60d751d7bc39788ea08dda274a5bc6c280a4ae2
Luiz Machado <guimaduarte2222@gmail.com>
2025-09-18 01:48:27 -0300

    fix: tests e2e fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
-    environment: 'node',
+    environment:
+      './prisma/vitest-environment-prisma/prisma-test-environment.ts',
   },
 })
